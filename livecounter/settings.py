1cf750313667f3985b2897cb5988d9cdeb21c6db
lumuslens <lumuslenstemp@gmail.com>
2026-01-24 10:44:40 +0530

    --

--- livecounter/settings.py
+++ livecounter/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'livecounter.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-      "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    #   "default": dj_database_url.config(
+    #     default=os.environ.get("DATABASE_URL")
+    # )
 }
 
 
