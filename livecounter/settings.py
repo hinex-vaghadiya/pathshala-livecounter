2f945d7287bfd9a2a17a9bbb2865f36c3e6aaac1
lumuslens <lumuslenstemp@gmail.com>
2026-01-24 10:34:54 +0530

    allowed host

--- livecounter/settings.py
+++ livecounter/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
