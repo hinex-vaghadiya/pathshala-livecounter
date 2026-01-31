cdb6782abd11a1c1d6d4ff72e23b8d552b6f1810
lumuslens <lumuslenstemp@gmail.com>
2026-01-31 09:25:50 +0530

    database url added in env

--- livecounter/settings.py
+++ livecounter/settings.py
@@ -87,14 +87,15 @@ WSGI_APPLICATION = 'livecounter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL=os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    #    "default": dj_database_url.config(
-    #     default="postgresql://livecounter_postgress_1joq_user:8pQ8VBypx5PDpf8KDIraVjnfhbjT9xwq@dpg-d5q5l04oud1c73duf05g-a.oregon-postgres.render.com/livecounter_postgress_1joq"
-    # )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+       "default": dj_database_url.config(
+        default=DATABASE_URL
+       )
 }
 
 
